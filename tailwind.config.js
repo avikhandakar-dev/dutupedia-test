fffa5d21d5a4f314c1458b6a74e71ed7ae378b90
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-04-13 21:18:03 +0600

    add file:

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      boxShadow: {
+        '3xl': '0px 0px 15px -3px rgba(0,0,0,0.12)',
+        '4xl': '0px 0px 15px -5px rgba(0,0,0,0.12)',
+        '5xl': '0px 0px 15px -6px rgba(0,0,0,0.12)',
+      }
+    },
+  },
+}
