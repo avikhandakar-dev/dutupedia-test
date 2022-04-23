5efb93c3d8d45b06174466186cff5ccd2d329d79
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-04-23 19:47:20 +0600

    upadte file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const route = useRouter();
+  // console.log(route.pathname === '/dashboard_1' ? 'true' : 'false');
+  const routeArray = route.pathname.split('/');
+  return (
+    <>
+      <Provider store={store}>
+        {/* {routeArray[1] === 'multiple_dashboard' && (
+          <>
+            hello
+          </>
+        )} */}
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  )
 }
 
 export default MyApp
