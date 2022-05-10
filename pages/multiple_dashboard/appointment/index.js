ae3426a2e10be2f957a846dae56653ea3199a503
Avi Khandakar <avi@Avis-MacBook-Air.local>
2022-05-10 16:39:04 +0600

    init

--- pages/multiple_dashboard/appointment/index.js
+++ pages/multiple_dashboard/appointment/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import AppoinmentHeader from '../../../Components/multiple_dashboard/Appointment/AppoinmentHeader';
+import Nofile from '../../../Components/multiple_dashboard/Appointment/Nofile';
+import OfflineTime from '../../../Components/multiple_dashboard/Appointment/OfflineTime';
+import OnlineSearch from '../../../Components/multiple_dashboard/Appointment/OnlineSearch';
+import RequestCard from '../../../Components/multiple_dashboard/Appointment/RequestCard';
 
 function index() {
-   return (
-      <div>index</div>
-   )
+    return (
+        <>
+            <AppoinmentHeader />
+            <Nofile />
+            <OfflineTime />
+            <OnlineSearch />
+            <RequestCard />
+        </>
+    );
 }
 
-export default index
+export default index;
