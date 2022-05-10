7668b251f362d288db2511240233ddd58f0d817c
Avi Khandakar <avi@Avis-MacBook-Air.local>
2022-05-10 16:46:42 +0600

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,28 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { multipleDashboardData } from '../Components/navbar/NavigationData';
 import SideNavBar from '../Components/navbar/sidebar';
 import TopNavBar from '../Components/navbar/topbar';
-import store from '../redux/store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+    const route = useRouter();
+    const routeArray = route.pathname.split('/');
 
-  const route = useRouter();
-  const routeArray = route.pathname.split('/');
+    return (
+        <>
+            {routeArray[1] === 'multiple_dashboard' ? (
+                <>
+                    <TopNavBar />
+                    <SideNavBar data={multipleDashboardData} />
+                </>
+            ) : (
+                ''
+            )}
 
-  return (
-    <>
-      <Provider store={store}>
-
-        {
-          routeArray[1] === "multiple_dashboard" ?
-            <>
-              <TopNavBar />
-              <SideNavBar data={multipleDashboardData} />
-            </> : ""
-        }
-
-
-        <Component {...pageProps} />
-
-      </Provider>
-    </>
-  )
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
